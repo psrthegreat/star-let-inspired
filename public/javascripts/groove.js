1cfd6435140cc87d3fb88e2ffb68c19771269922
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-25 03:02:38 -0700

    changed to localhost, adds dependency

--- public/javascripts/groove.js
+++ public/javascripts/groove.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://www.vocalet.com/');
+var socket = io.connect('http://localhost:5000/');
 socket.on('data', function (data) {
     $("#current").text(data.cur);
  });
--- public/javascripts/groove2.js
+++ public/javascripts/groove2.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://www.vocalet.com/');
+var socket = io.connect('http://localhost:5000/');
 
 var player;
 
