3f35043a27052abd9be48785c8a24de859239b04
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-25 02:47:11 -0700

    changes to use remote host

--- public/javascripts/groove.js
+++ public/javascripts/groove.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('http://vocalet.com:3000/');
 socket.on('data', function (data) {
     $("#current").text(data.cur);
  });
