e94d7fc420a7be39ca2db2e5d30467d354a1b64a
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-25 02:46:05 -0700

    diff js for dj and nondj

--- public/javascripts/groove2.js
+++ public/javascripts/groove2.js
@@ -1,46 +1,54 @@
+var socket = io.connect('http://localhost:3000/');
+
 var player;
+
 function onYouTubePlayerAPIReady() {
 	player = new YT.Player('player', {
 		height: '300',
 		width: '100%',
 		playerVars: {'autohide': 1,
-		'controls': 2,
-		'enablejsapi': 1,
-		'fs': 0,
-		'iv_load_policy': 3,
-		'modestbranding': 1,
-		'rel': 0,
-		'showinfo': 0,
-		'autoplay': 0},
+					 'controls': 2,
+					 'enablejsapi': 1,
+					 'fs': 0,
+					 'iv_load_policy': 3,
+					 'modestbranding': 1,
+					 'rel': 0,
+					 'showinfo': 0,
+					 'autoplay': 0},
 		'events': {
 			'onReady': onReady,
 			'onStateChange': onPlayerStateChange
 		}
-		});
+	});
+}
+
+function change(){
+	var next = $('#imp').val().split('\n')[0];
+	searchYoutube(next, 1, "", function(title, id){
+		player.loadVideoById(id);
+		var cur = $('#imp').val();
+		var next = cur.substring(cur.indexOf('\n') + 1);
+		$('#imp').val(next);
+
+		socket.emit('curbroad', {'cur': title});
+		$('#current').text(title);
+	});
 }
 
 function onReady(event){
-	searchYoutube(next, 1, "", function(id){
-			player.loadVideoById(id);
-			var cur = $('#imp').val();
-			var a = cur.substring(cur.indexOf('\n') + 1);
-			$('#imp').val(a);
-			});
+	change();
 }
 
 // when video ends
 function onPlayerStateChange(event) {        
 	if(event.data === 0) {          
-		searchYoutube(next, 1, "", function(id){
-			player.loadVideoById(id);
-			var cur = $('#imp').val();
-			var a = cur.substring(cur.indexOf('\n') + 1);
-			$('#imp').val(a);
-			});
+		change();
 	}
 }
 
-var next;
+$('#btny').click(function(){
+	change();
+});
 
 
 function searchYoutube(query, limit, tab, callback) {
@@ -55,18 +63,9 @@ function searchYoutube(query, limit, tab, callback) {
 			var imageSource = $(this).find("media\\:thumbnail, thumbnail").first().attr('url');
 			var link = $(this).find("id").text();
 			var videoID = link.substr(link.search('video:') + 6);
-			callback(videoID);
+			callback(title, videoID);
 		});
 	}).fail(function(data) {
 		$('#' + tab).html("Could not get your videos. Please check your internet connection.");
 	});
 }
-
-$(window).on('load', function(){
-	next = $('#imp').val().split('\n')[0];
-	})
-$('#imp').on('input propertychange', function() {
-	next = $(this).val().split('\n')[0];
-});
-
-
