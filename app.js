cdea76e15d21553285282720d9e82b72fe7245aa
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-08-09 14:07:20 -0700

    adds mongoose, memorisation, iTunes request

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var express = require('express'),
 	http = require('http'),
-	path = require('path');
+	path = require('path'),
+  mongoose = require ('mongoose'),
+  request = require('request'),
+  parseString = require('xml2js').parseString,
+  memoize = require('memoizee');
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/vocalet';
+
+var getSongs = function(callback){
+  request("https://itunes.apple.com/us/rss/topsongs/limit=15/xml", function(err, response, body){
+    parseString(body, function (err, result) {
+      var entries = result.feed.entry;
+      var titles = []
+      for(var i =0; i < entries.length; i++){
+        titles.push(entries[i].title[0]);
+      }
+      callback(titles);
+    });
+  });
+}
+
+memoized = memoize(getSongs, {primitive: true, async: true,  maxAge: 10000000});
 
 var app = express();
-
-
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -24,17 +46,29 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+app.get('/try', function(req, res){
+  memoized(function(response){
+    res.send(response);
+  });
+});
+
 app.get('/', function (req, res){
 	res.render('login');
 });
 app.get('/main', function (req, res){
-	res.render('main');
+  getSongs(function(results){
+    res.render('main', {results: results});
+  });
 });
 
 app.get('/groove', function(req, res){
   console.log(req.query.dj);
   res.render('groove', {dj: req.query.dj});
 });
+
+app.get('/songs', function(req, res){
+});
+
 
 var s = http.createServer(app);
 s.listen(app.get('port'), function(){
@@ -48,4 +82,12 @@ io.on('connection', function (socket) {
     console.log(data)
      socket.broadcast.emit('data', { cur: data.cur });
   });
+});
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
 });
